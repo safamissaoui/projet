6ff2985e30ce98c327de653f67fd6a01823e8a73
safa <missaouisafa@yahoo.frm>
2023-07-09 10:55:28 +0100

    first commit

---
+++ acceuil.js
@@ -0,0 +1,9 @@
+alert("are you a professional")
+function validateForm(e) {
+    e.preventDefault()
+    var email = document.loginForm.email.value
+    
+   if (name.length==0)
+    return alert(`e-mail is required`)
+   
+   }
---
+++ contact.js
@@ -0,0 +1,14 @@
+function validateForm(e) {
+    e.preventDefault()
+    var firstname = document.loginForm.name.value
+    
+   if (name.length==0)
+    return alert(`name is required`)
+    e.preventDefault()
+    var lastname = document.loginForm.name.value
+    
+   if (name.length==0)
+    return alert(`last name is required`)
+   
+   
+   }
